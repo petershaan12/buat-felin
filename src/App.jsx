5ec05de06204c8c75930147544129e1c176deb6e
petershaan12 <2181002@unai.edu>
2024-11-07 00:32:30 +0700

    bucinn banget

---
+++ src/App.jsx
@@ -0,0 +1,244 @@
+import { useState } from "react";
+import Awal from "./Awal";
+
+function Card1({ onClick }) {
+  return (
+    <div className="pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/1.gif" className="mx-auto h-36" />
+          <p>Hallo Sayang 💖</p>
+          <div className="btn">
+            <button onClick={onClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card2({ onClick }) {
+  return (
+    <div className="flex items-center justify-center pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/2.gif" className="mx-auto h-36" />
+          <p>Coba tebak deh siapa yang hari ini ulang taun? cluenya adalah F</p>
+          <div className="btn">
+            <button onClick={onClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card3({ onClick }) {
+  return (
+    <div className="flex items-center justify-center pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/3.gif" className="mx-auto h-36" />
+          <p>Yeayyyy betul pacar aku yg paling cantikk sedunia</p>
+          <div className="btn">
+            <button onClick={onClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card4({ onClick }) {
+  return (
+    <div className="flex items-center justify-center pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/4.gif" className="mx-auto h-36" />
+          <p>Selamat Ulang Tahun Pacar Aku yang cantik</p>
+          <div className="btn">
+            <button onClick={onClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card5({ onClick }) {
+  return (
+    <div className="flex items-center justify-center pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/5.gif" className="mx-auto h-36" />
+          <p>
+            Semangat Kuliahnya, Nyanyinya, Himanya dan semuanya banyakkkk...,
+            Pokoknya tetap setia selalu ya dan sayang orang tua dan Tuhan dan
+            aku hihihi
+          </p>
+          <div className="btn">
+            <button onClick={onClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card6({ onClick }) {
+  return (
+    <div className="flex items-center justify-center pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/6.gif" className="mx-auto h-36" />
+          <p>
+            Udah deh gitu aja maaf singkat krn yang lainnya dalam doa dan aku
+            mau tidur deh byee
+          </p>
+          <div className="btn">
+            <button onClick={onClick}>OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Card7({ onClick }) {
+  return (
+    <div className="flex items-center justify-center pu-ctnr pu-ctn">
+      <div className="pu bg-white">
+        <div className="t">
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div className="c">
+          <img src="/us.jpg" className="mx-auto h-36" />
+          <p>💖🥰</p>
+          <div className="btn">
+            <button onClick={onClick}>ulang lagi</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+
+  const cards = [
+    <Awal key="awal" onClick={() => handleNextCard()} />,
+    <Card1 key="card1" onClick={() => handleNextCard()} />,
+    <Card2 key="card2" onClick={() => handleNextCard()} />,
+    <Card3 key="card3" onClick={() => handleNextCard()} />,
+    <Card4 key="card4" onClick={() => handleNextCard()} />,
+    <Card5 key="card5" onClick={() => handleNextCard()} />,
+    <Card6 key="card6" onClick={() => handleNextCard()} />,
+    <Card7 key="card7" onClick={() => handleNextCard()} />,
+  ];
+
+  const handleNextCard = () => {
+    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % cards.length);
+  };
+
+  return (
+    <div className="bg-[#ffcdc4] flex items-center justify-center h-screen">
+      <div className="bg">
+        <div className="ly">
+          <div className="v">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="h">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          {cards[currentCardIndex]}
+          <img src="/bintang.svg" id="str" className="bg-tings" />
+          <img src="/bintang.svg" id="str" className="bg-tings" />
+          <img src="/bintang.svg" id="str" className="bg-tings" />
+          <img src="/stars.svg" id="strr" className="bg-tings" />
+          <img src="/smile.svg" id="sml" className="bg-tings" />
+          <img src="/love.svg" id="lv" className="bg-tings" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Awal.jsx
@@ -0,0 +1,13 @@
+export default function Awal({ onClick }) {
+  return (
+    <>
+      <div className="stp">
+        <p>
+          Anyongg, ada paket buuuu
+          <img src="https://dekatutorial.github.io/ct/sudut.svg" id="" />
+        </p>
+      </div>
+      <img src="/mail.svg" id="ml" onClick={onClick}  className="bg-tings"/>
+    </>
+  );
+}
